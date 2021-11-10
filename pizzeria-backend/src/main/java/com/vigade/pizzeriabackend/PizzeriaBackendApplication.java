b955f9715f3eba86ee5275aa74cf488c37d6f4b4
Jose Pascual Manzano Perez <josepascual.manzano@gmail.com>
2021-11-10 11:29:34 +0100

    Springboot project creation.

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/PizzeriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vigade.pizzeriabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaBackendApplication.class, args);
+	}
+
+}
