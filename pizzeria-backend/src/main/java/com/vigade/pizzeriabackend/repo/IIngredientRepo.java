0c4e3334e67ce56dccf21af289d4a5acc9ecbefa
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-15 00:09:31 +0100

    Create IIngredientRepo.java

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/repo/IIngredientRepo.java
@@ -0,0 +1,15 @@
+package com.vigade.pizzeriabackend.repo;
+
+import com.vigade.pizzeriabackend.model.Ingredient;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface IIngredientRepo {
+    Mono<Ingredient> register(Ingredient ingredient);
+    Mono<Ingredient> modify(Ingredient ingredient);
+    Flux<Ingredient> list();
+    Mono<Ingredient> listById(Integer id);
+    Mono<Void> delete(Integer id);
+    
+}
