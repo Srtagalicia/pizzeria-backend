bfe9ae203711d2b4d817c11adfe6db7d81273cbc
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-15 00:14:27 +0100

    Created a function to register ingredients

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,11 @@ public class IngredientController {
     @GetMapping
     public Flux<Ingredient> list() {
         return repo.list();
+    }
+    //para registrar ingredientes
+    @PostMapping
+    public Mono<Ingredient> register(@RequestBody Ingredient ingredient) {
+        return repo.register(ingredient);
     }
     
 }
