557a5e1d7620ba9cdd91978b39803b2fe9ce9f62
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-15 00:15:30 +0100

    add a publisher mono import

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @RestController
