7abe856c021a17f71a17cc3e9bfa754069ee7f9a
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-15 00:15:00 +0100

    Created a method to modify ingredients

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,10 @@ public class IngredientController {
     //para registrar ingredientes
     @PostMapping
     public Mono<Ingredient> register(@RequestBody Ingredient ingredient) {
+        return repo.register(ingredient);
+    }
+    @PutMapping
+    public Mono<Ingredient> modify(@RequestBody Ingredient ingredient) {
         return repo.register(ingredient);
     }
     
