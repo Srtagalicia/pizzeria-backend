e725352cf9ef6cb9f5303491ba5eeb4e36bd239b
Jose Pascual Manzano Perez <josepascual.manzano@gmail.com>
2021-11-16 11:21:41 +0100

    Implementation of Getters and Setters, Input validation, new UUID strategy and id type, name and price not null.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/IngredientDTOOutput.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/IngredientDTOOutput.java
@@ -1,7 +1,10 @@
 package com.vigade.pizzeriabackend.application;
 
 import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class IngredientDTOOutput {
     private UUID id;
     private String name;
--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/Ingredient.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/Ingredient.java
@@ -5,23 +5,30 @@ import javax.persistence.Entity;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import org.hibernate.annotations.GenericGenerator;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Table(name = "Ingredients")
+@Getter @Setter
 public class Ingredient {
     @Id
-    @Column(
-        name="id",
-        updatable=false,
-        nullable=false
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
     )
-    private UUID id = UUID.randomUUID();
-    @Getter
-    @Setter
+    @Column(
+        name = "id",
+        updatable = false,
+        nullable = false,
+        columnDefinition = "varbinary(16)"
+    )
+    private UUID id;
+    @Column(nullable = false)
     private String name;
-    @Getter
-    @Setter
+    @Column(nullable = false)
     private float price;
 }
