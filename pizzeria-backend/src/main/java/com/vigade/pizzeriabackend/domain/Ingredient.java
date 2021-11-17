2babfc0ee045278f21ac9f1511ef4cff53283765
Fran <franciscopereatora@gmail.com>
2021-11-17 11:44:38 +0100

    add annotations and price variable to ingredient

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/Ingredient.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/Ingredient.java
@@ -1,17 +1,31 @@
 package com.vigade.pizzeriabackend.domain;
 
-import lombok.*;
+import java.util.UUID;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.*;
+@Table (("ingredients"))
+@Data
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Ingredient {
     @Getter
     @Setter
-    private int ingredientId;
+    @Id
+    @Column("id")
+    private int id;
     @Getter
     @Setter
-    private String name;
+    @Column("name")
+    private String ingredientName;
+    @Getter
+    @Setter
+    @Column("price")
+    private float price;
 
     
 
