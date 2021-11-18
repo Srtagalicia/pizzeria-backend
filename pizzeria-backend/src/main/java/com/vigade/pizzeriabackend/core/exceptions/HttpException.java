a2c16d6e1b5a617491c844ca3c43462632579120
manuela-agrom <manuela071997@gmail.com>
2021-11-18 13:26:48 +0100

    HttpException and NotFoundException made

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/exceptions/HttpException.java
@@ -0,0 +1,12 @@
+package com.vigade.pizzeriabackend.core.exceptions;
+
+public class HttpException extends RuntimeException {
+    private Integer code;
+    public HttpException(Integer code, String message){
+        super(message);
+        this.code = code;
+    }
+    public Integer getCode(){
+        return this.code;
+    }
+}
