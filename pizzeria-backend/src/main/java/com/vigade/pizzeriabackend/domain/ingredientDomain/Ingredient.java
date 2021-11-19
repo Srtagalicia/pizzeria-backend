fe0a1f5d51abd568154faea706eb00e091a076ff
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-19 09:44:52 +0100

    Ingredient entity updated and relocated.
    Removed unnecessary imports.
    Inheritance from Entitybase.
    Changed type of "price" from "float" to "BigDecimal" in order to have more precision.
    It is relocated to the "ingredientDomain" folder.

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/ingredientDomain/Ingredient.java
@@ -0,0 +1,19 @@
+package com.vigade.pizzeriabackend.domain.ingredientDomain;
+
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import java.math.BigDecimal;
+import com.vigade.pizzeriabackend.core.EntityBase;
+
+@Table("ingredients")
+public @NoArgsConstructor @Getter @Setter class Ingredient extends EntityBase {
+
+    @Column("name")
+    private String name;
+
+    @Column("price")
+    private BigDecimal price;
+}
