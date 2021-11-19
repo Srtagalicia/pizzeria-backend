9d38f7440988e573afe680e8f274bdca43dbf619
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-19 09:26:43 +0100

    IngredientController controller updated imports.
    Updated imports because of files relocation and removal of unnecessary imports.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -1,23 +1,16 @@
 package com.vigade.pizzeriabackend.controller;
 
 import javax.validation.Valid;
-import com.vigade.pizzeriabackend.application.IngredientApplication;
-import com.vigade.pizzeriabackend.application.IngredientDTOInput;
-import com.vigade.pizzeriabackend.application.IngredientDTOOutput;
-import com.vigade.pizzeriabackend.domain.Ingredient;
-import com.vigade.pizzeriabackend.infrastructure.IngredientRepository;
-
+import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientApplication;
+import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientDTOInput;
+import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientDTOOutput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import reactor.core.publisher.Mono;
 
 @RestController
