e2a767c04de77ed510a197c47da0e68b25f2fe28
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-23 12:59:23 +0100

    "getAllIngredients" method implemented.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientApplication;
 import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientDTOInput;
 import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientDTOOutput;
+import com.vigade.pizzeriabackend.domain.ingredientDomain.IngredientProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -50,5 +52,11 @@ public class IngredientController {
 	@ResponseStatus(HttpStatus.OK)
 	public Mono<Void> deleteIngredient(@PathVariable UUID id) {
 		return this.ingredientApplication.delete(id);
+	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public Flux<IngredientProjection> getAllIngredients() {
+		return this.ingredientApplication.getAll();
 	}
 }
