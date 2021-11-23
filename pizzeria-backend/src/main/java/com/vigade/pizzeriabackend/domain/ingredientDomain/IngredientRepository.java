aaa6c658ed802bff085a2c1c3cb8bccf1637fb34
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-23 12:55:25 +0100

    "getAll" method included.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/ingredientDomain/IngredientRepository.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/ingredientDomain/IngredientRepository.java
@@ -2,10 +2,12 @@ package com.vigade.pizzeriabackend.domain.ingredientDomain;
 
 import java.util.UUID;
 import com.vigade.pizzeriabackend.core.functionalInterfaces.FindById;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface IngredientRepository extends FindById<Ingredient, UUID> {
     public Mono<Ingredient> add(Ingredient ingredient);
     public Mono<Ingredient> update(Ingredient ingredient);
     public Mono<Void> delete(Ingredient ingredient);
+    public Flux<IngredientProjection> getAll();
 }
