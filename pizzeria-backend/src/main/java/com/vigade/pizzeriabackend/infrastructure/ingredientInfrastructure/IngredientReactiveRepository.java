a384edf1f8d29be78efc40839eca6ab3dda9203f
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-23 12:55:01 +0100

    "findAllIngredients" query method included.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/ingredientInfrastructure/IngredientReactiveRepository.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/ingredientInfrastructure/IngredientReactiveRepository.java
@@ -2,8 +2,15 @@ package com.vigade.pizzeriabackend.infrastructure.ingredientInfrastructure;
 
 import java.util.UUID;
 import com.vigade.pizzeriabackend.domain.ingredientDomain.Ingredient;
+import com.vigade.pizzeriabackend.domain.ingredientDomain.IngredientProjection;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 
 public interface IngredientReactiveRepository extends ReactiveCrudRepository<Ingredient, UUID> {
     
+    @Query("SELECT id, name, price FROM ingredients;")
+    @Transactional(readOnly = true)
+    public Flux<IngredientProjection> findAllIngredients();
 }
