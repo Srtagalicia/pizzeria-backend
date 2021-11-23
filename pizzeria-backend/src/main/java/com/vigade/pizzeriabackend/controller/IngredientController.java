16bedf805e17ea360edc7919d81a8b422a8b4529
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-23 12:57:53 +0100

    "DeleteMapping" import included.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -6,6 +6,7 @@ import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientDT
 import com.vigade.pizzeriabackend.application.ingredientApplication.IngredientDTOOutput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
