52764ac216faaeb3054e5332f36262d4272c5df0
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-23 12:53:18 +0100

    "delete" method included.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/ingredientApplication/IngredientApplication.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/ingredientApplication/IngredientApplication.java
@@ -7,4 +7,5 @@ public interface IngredientApplication {
     public Mono<IngredientDTOOutput> add(IngredientDTOInput dto);
     public Mono<IngredientDTOOutput> get(UUID id);
     public Mono<IngredientDTOOutput> update(UUID id, IngredientDTOInput dto);
+    public Mono<Void> delete(UUID id);
 }
