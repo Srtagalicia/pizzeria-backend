e8655397425af3f1d6a1995f74752f58cde636c7
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-24 09:48:30 +0100

    "getAllIngredients" method with input parameters name, limit and offset implemented.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/IngredientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -56,7 +57,11 @@ public class IngredientController {
 
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	public Flux<IngredientProjection> getAllIngredients() {
-		return this.ingredientApplication.getAll();
+	public Flux<IngredientProjection> getAllIngredients(
+		@RequestParam(required = false) String name,
+		@RequestParam(defaultValue = "10") Integer limit,
+		@RequestParam(defaultValue = "0") Integer offset
+	) {
+		return this.ingredientApplication.getAll(name, limit, offset);
 	}
 }
