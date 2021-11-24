6df1315c2dfad22b42a1dcda5eb6db952d72fe10
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-24 09:40:35 +0100

    "findAllIngredients" query method with filter by name, limit and offset

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/ingredientInfrastructure/IngredientReactiveRepository.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/ingredientInfrastructure/IngredientReactiveRepository.java
@@ -4,13 +4,14 @@ import java.util.UUID;
 import com.vigade.pizzeriabackend.domain.ingredientDomain.Ingredient;
 import com.vigade.pizzeriabackend.domain.ingredientDomain.IngredientProjection;
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 
 public interface IngredientReactiveRepository extends ReactiveCrudRepository<Ingredient, UUID> {
     
-    @Query("SELECT id, name, price FROM ingredients;")
+    @Query("SELECT id, name, price FROM ingredients WHERE :name IS NULL OR name LIKE CONCAT('%', :name, '%') LIMIT :limit OFFSET :offset;")
     @Transactional(readOnly = true)
-    public Flux<IngredientProjection> findAllIngredients();
+    public Flux<IngredientProjection> findAllIngredients(@Param(value="name") String name, @Param(value="limit") Integer limit, @Param(value="offset") Integer offset);
 }
