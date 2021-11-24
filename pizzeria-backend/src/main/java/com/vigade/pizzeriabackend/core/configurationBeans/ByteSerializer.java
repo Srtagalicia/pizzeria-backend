b4b90756bd0b5fcb8a3c797454d016e99c53b3bf
Fran Perea <franciscopereatora@gmail.com>
2021-11-25 00:28:12 +0100

    Create a Byte serializer to upload images using arrays of bytes

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/configurationBeans/ByteSerializer.java
@@ -0,0 +1,19 @@
+package com.vigade.pizzeriabackend.core.configurationBeans;
+
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ByteSerializer implements RedisSerializer<byte[]> {
+    @Override
+    public byte[] deserialize(byte[] bytes) throws SerializationException {
+        return bytes;
+    }
+
+    @Override
+    public byte[] serialize(byte[] bytes) throws SerializationException {
+        return bytes;
+    }
+}
