f81136d305e16da27e23b0afddbc073e66ec573b
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-25 10:22:40 +0100

    Fields validation implemented.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/ingredientApplication/IngredientDTOInput.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/ingredientApplication/IngredientDTOInput.java
@@ -3,8 +3,19 @@ package com.vigade.pizzeriabackend.application.ingredientApplication;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Validated
 public @Getter @Setter class IngredientDTOInput {
+    
+    @NotBlank(message="name is mandatory") @Size(min=2, max=45)
     private String name;
+
+    @NotNull(message="price is mandatory") @Digits(integer=3, fraction=2) @DecimalMin(value="0.0", inclusive=false)
     private BigDecimal price;
 }
