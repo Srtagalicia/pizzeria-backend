4b296c209a32d3e293091f39a9bb3230abddb34a
Fran Perea <franciscopereatora@gmail.com>
2021-11-25 01:22:32 +0100

    Create a data transfer object to create or update Images

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/CreateOrUpdateImageDTO.java
@@ -0,0 +1,12 @@
+package com.vigade.pizzeriabackend.application.imageAplication;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateOrUpdateImageDTO {
+    public byte[] data;
+}
