a3f705a482f1e70e02dd01e504d188395744ad15
Fran Perea <franciscopereatora@gmail.com>
2021-11-25 01:24:19 +0100

    Implement Image add function to add an image to the redis database

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplicationImpl.java
@@ -0,0 +1,32 @@
+package com.vigade.pizzeriabackend.application.imageAplication;
+
+import java.util.UUID;
+
+import com.vigade.pizzeriabackend.domain.imageDomain.Image;
+import com.vigade.pizzeriabackend.infrastructure.imageRepository.ImageReactiveRepository;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class ImageApplicationImpl implements ImageApplication {
+    private final ImageReactiveRepository imageRepository;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ImageApplicationImpl(final ImageReactiveRepository imageRepository, final ModelMapper modelMapper) {
+        this.imageRepository = imageRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public Mono<ImageDTO> add(CreateOrUpdateImageDTO dto) {
+        Image image = modelMapper.map(dto, Image.class);
+        image.setId(UUID.randomUUID());
+        image.setThisNew(true);
+        return this.imageRepository.add(image)
+                .flatMap(monoImage -> Mono.just(this.modelMapper.map(monoImage, ImageDTO.class)));
+    }
+}
