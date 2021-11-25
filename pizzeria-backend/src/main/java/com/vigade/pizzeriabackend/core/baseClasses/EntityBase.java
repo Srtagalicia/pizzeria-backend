fcc15a09e5bed11768fa5361cd0887e4c4fbf061
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-25 10:04:38 +0100

    Added "validate()" method.
    The method throws an exception if there are any constraint violations in the class.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/baseClasses/EntityBase.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/baseClasses/EntityBase.java
@@ -1,12 +1,20 @@
 package com.vigade.pizzeriabackend.core.baseClasses;
 
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.validation.annotation.Validated;
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+import javax.validation.Validator;
+import javax.validation.ConstraintViolation;
+import com.vigade.pizzeriabackend.core.exceptions.BadRequestException;
 
+@Validated
 public @Getter @Setter abstract class EntityBase implements Persistable<UUID> {
     
     @Id
@@ -14,6 +22,19 @@ public @Getter @Setter abstract class EntityBase implements Persistable<UUID> {
 
     @Transient
     protected boolean isThisNew = false;
+
+    public void validate() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator= factory.getValidator();
+        Set<ConstraintViolation<EntityBase>> violations = validator.validate(this);
+        if(!violations.isEmpty()) {
+            BadRequestException badRequestException = new BadRequestException();
+            for(ConstraintViolation<EntityBase> violation : violations) {
+                badRequestException.addException(violation.getPropertyPath().toString(), violation.getMessage());
+            }
+            throw badRequestException;
+        }
+    }
 
     @Override
     public boolean isNew() {
