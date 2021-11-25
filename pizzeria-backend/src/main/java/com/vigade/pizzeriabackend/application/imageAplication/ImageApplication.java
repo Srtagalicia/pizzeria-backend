d25a3c837af6876cc24de0cc61e89bb0c2717301
Fran Perea <franciscopereatora@gmail.com>
2021-11-25 01:23:17 +0100

    Create an interface to add an image using Mono

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplication.java
@@ -0,0 +1,8 @@
+package com.vigade.pizzeriabackend.application.imageAplication;
+
+import reactor.core.publisher.Mono;
+
+public interface ImageApplication {
+    public Mono<ImageDTO> add(CreateOrUpdateImageDTO dto);
+
+}
