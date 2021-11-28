dfcac2881277697d11452eae8687c1a8479d0dbf
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-28 16:06:57 +0100

    Added "existsByEmail" query method.
    The "existsByEmail" method is needed for validation.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/userInfrastructure/UserReactiveRepository.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/userInfrastructure/UserReactiveRepository.java
@@ -2,8 +2,15 @@ package com.vigade.pizzeriabackend.infrastructure.userInfrastructure;
 
 import java.util.UUID;
 import com.vigade.pizzeriabackend.domain.userDomain.User;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 
 public interface UserReactiveRepository extends ReactiveCrudRepository<User, UUID> {
     
+    @Query("SELECT CASE WHEN COUNT(email)>0 THEN true ELSE false END FROM users WHERE email = :email;")
+    @Transactional(readOnly = true)
+    public Mono<Integer> existsByEmail(@Param(value="email") String email);
 }
