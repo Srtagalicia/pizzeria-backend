207cdca204db3a415c34aa6c8406703278263794
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-28 16:05:54 +0100

    Added "existsByField" method via interface extension.
    The "existsByField" method is needed for validation.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/userDomain/UserRepository.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/domain/userDomain/UserRepository.java
@@ -2,8 +2,9 @@ package com.vigade.pizzeriabackend.domain.userDomain;
 
 import java.util.UUID;
 import com.vigade.pizzeriabackend.core.functionalInterfaces.FindById;
+import com.vigade.pizzeriabackend.core.functionalInterfaces.ExistsByField;
 import reactor.core.publisher.Mono;
 
-public interface UserRepository extends FindById<User, UUID>{
+public interface UserRepository extends FindById<User, UUID>, ExistsByField{
     public Mono<User> add(User user);
 }
