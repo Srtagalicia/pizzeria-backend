01b86825be5a29bfd4aa36364182d9c3de78a16f
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 03:10:42 +0100

    create a get endpoint in imagecontroller

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/ImageController.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/controller/ImageController.java
@@ -1,12 +1,18 @@
 package com.vigade.pizzeriabackend.controller;
 
 import java.io.IOException;
-import com.vigade.pizzeriabackend.application.imageAplication.ImageDTOInput;
+import java.util.UUID;
+
 import com.vigade.pizzeriabackend.application.imageAplication.ImageApplication;
 import com.vigade.pizzeriabackend.application.imageAplication.ImageDTO;
+import com.vigade.pizzeriabackend.application.imageAplication.ImageDTOInput;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +39,11 @@ public class ImageController {
         createOrUpdateImageDTO.setData(file.getBytes());
         return imageApplication.add(createOrUpdateImageDTO);
        
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "/{id}")
+    public Mono<ResponseEntity<ImageDTO>> getImage(@PathVariable UUID id) {
+        return this.imageApplication.getImageRedis(id).map(image -> ResponseEntity.ok(image)).defaultIfEmpty(ResponseEntity.notFound().build());
     }
    
 }
