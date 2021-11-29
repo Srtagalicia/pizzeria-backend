2f5b7e39d57fcd7a62e64891402d3c32db266935
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 03:09:17 +0100

    Created get function for images in image repository interface

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/imageRepository/ImageReactiveRepository.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/imageRepository/ImageReactiveRepository.java
@@ -1,9 +1,12 @@
 package com.vigade.pizzeriabackend.infrastructure.imageRepository;
 
+import java.util.UUID;
+
 import com.vigade.pizzeriabackend.domain.imageDomain.Image;
 
 import reactor.core.publisher.Mono;
 
 public interface ImageReactiveRepository {
     public Mono<Image> add(Image image);
+    public Mono<Image> getImageRedis(UUID uuid);
 }
