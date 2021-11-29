830289b842079bb3877ace94262b34d9ce286ab2
Fran Perea <franciscopereatora@gmail.com>
2021-11-29 13:10:01 +0100

    Refactor ImageRepository and format the code correctly

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplication.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplication.java
@@ -1,11 +1,9 @@
 package com.vigade.pizzeriabackend.application.imageAplication;
 
 import java.util.UUID;
-
 import reactor.core.publisher.Mono;
 
 public interface ImageApplication {
     public Mono<ImageDTO> add(ImageDTOInput dto);
     public Mono<ImageDTO> getImageRedis(UUID id);
-
 }
--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplicationImpl.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplicationImpl.java
@@ -1,39 +1,44 @@
 package com.vigade.pizzeriabackend.application.imageAplication;
 
 import java.util.UUID;
-
 import com.vigade.pizzeriabackend.core.baseClasses.ApplicationBase;
 import com.vigade.pizzeriabackend.domain.imageDomain.Image;
-import com.vigade.pizzeriabackend.infrastructure.imageRepository.ImageReactiveRepository;
-
+import com.vigade.pizzeriabackend.domain.imageDomain.ImageRepository;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import reactor.core.publisher.Mono;
 
 @Service
 public class ImageApplicationImpl extends ApplicationBase<Image, UUID> implements ImageApplication {
-    private final ImageReactiveRepository imageRepository;
+    
+    private final ImageRepository imageRepository;
     private final ModelMapper modelMapper;
     private final Logger logger;
 
     @Autowired
-    public ImageApplicationImpl(final ImageReactiveRepository imageRepository, final ModelMapper modelMapper, final Logger logger) {
+    public ImageApplicationImpl(
+        final ImageRepository imageRepository, 
+        final ModelMapper modelMapper, 
+        final Logger logger
+    ) {
         super((id) -> imageRepository.getImageRedis(id));
         this.imageRepository = imageRepository;
         this.modelMapper = modelMapper;
         this.logger = logger;
     }
-//TODO: te deja añadir imagenes sin data
+
+    //TODO: impedir añadir imagenes sin data
     public Mono<ImageDTO> add(ImageDTOInput dto) {
         Image image = modelMapper.map(dto, Image.class);
         image.setId(UUID.randomUUID());
         image.setThisNew(true);
-        this.logger.info(serializeObject(image, "Image Added"));
-        return this.imageRepository.add(image)
-                .flatMap(monoImage -> Mono.just(this.modelMapper.map(monoImage, ImageDTO.class)));
+        
+        return this.imageRepository.add(image).flatMap(monoImage -> {
+            this.logger.info(serializeObject(monoImage, "Image Added"));
+            return Mono.just(this.modelMapper.map(monoImage, ImageDTO.class));
+        });
     }
 
     public Mono<ImageDTO> getImageRedis(UUID id){
