e14abf285849bc3517fb4c3acfda1967d99416c6
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-29 20:31:40 +0100

    Implemented Logger functionality.
    Implemented Logger functionality for "add", "update" and "delete" methods.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/ingredientApplication/IngredientApplicationImp.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/ingredientApplication/IngredientApplicationImp.java
@@ -10,18 +10,25 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
 
 @Service
 public class IngredientApplicationImp extends ApplicationBase<Ingredient, UUID> implements IngredientApplication {
-    
+
     private IngredientRepository ingredientRepository;
     private ModelMapper modelMapper;
+    private Logger logger;
 
     @Autowired
-    public IngredientApplicationImp(IngredientRepository ingredientRepository, ModelMapper modelMapper) {
+    public IngredientApplicationImp (
+        IngredientRepository ingredientRepository, 
+        ModelMapper modelMapper, 
+        Logger logger
+    ) {
         super(id -> ingredientRepository.findById(id));
         this.ingredientRepository = ingredientRepository;
         this.modelMapper = modelMapper;
+        this.logger = logger;
     }
 
     @Override
@@ -30,7 +37,10 @@ public class IngredientApplicationImp extends ApplicationBase<Ingredient, UUID> 
         ingredient.setId(UUID.randomUUID());
         ingredient.setThisNew(true);
         return ingredient.validate("name", ingredient.getName(), name -> this.ingredientRepository.existsByField(name))
-            .then(this.ingredientRepository.add(ingredient).flatMap(monoIngredient -> Mono.just(this.modelMapper.map(monoIngredient, IngredientDTOOutput.class))));
+                .then(this.ingredientRepository.add(ingredient).flatMap(monoIngredient -> {
+                    logger.info(this.serializeObject(monoIngredient, "Added Ingredient"));
+                    return Mono.just(this.modelMapper.map(monoIngredient, IngredientDTOOutput.class));
+                }));
     }
 
     @Override
@@ -44,18 +54,27 @@ public class IngredientApplicationImp extends ApplicationBase<Ingredient, UUID> 
             if(monoIngredient.getName().equals(ingredientDTOInput.getName())) {
                 this.modelMapper.map(ingredientDTOInput, monoIngredient);
                 monoIngredient.validate();
-                return this.ingredientRepository.update(monoIngredient).then(Mono.empty());
+                return this.ingredientRepository.update(monoIngredient).flatMap(updatedMonoIngredient -> {
+                    logger.info(this.serializeObject(updatedMonoIngredient, "Update Ingredient"));
+                    return Mono.empty();
+                });
             }
             this.modelMapper.map(ingredientDTOInput, monoIngredient);
             return monoIngredient.validate("name", monoIngredient.getName(), name -> this.ingredientRepository.existsByField(name))
-                .then(this.ingredientRepository.update(monoIngredient)).then(Mono.empty());
+                .then(this.ingredientRepository.update(monoIngredient)).flatMap(updatedMonoIngredient -> {
+                    logger.info(this.serializeObject(updatedMonoIngredient, "Update Ingredient"));
+                    return Mono.empty();
+                });
         });
     }
 
     @Override
     public Mono<Void> delete(UUID id) {
         return this.getById(id).flatMap(monoIngredient -> {
-            return this.ingredientRepository.delete(monoIngredient);
+            return this.ingredientRepository.delete(monoIngredient).flatMap(deletedMonoIngredient -> {
+                    logger.info(this.serializeObject(monoIngredient, "Delete Ingredient"));
+                    return Mono.empty();
+                });
         });
     }
 
