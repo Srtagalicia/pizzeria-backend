8a35541435b50a7e7c87804f26236f8c9a4a8c5b
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 01:05:58 +0100

    Divide redis configuration in 2 files, one for users and one for images

---
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/configurationBeans/UserRedisConfiguration.java
@@ -0,0 +1,23 @@
+package com.vigade.pizzeriabackend.core.configurationBeans;
+
+import com.vigade.pizzeriabackend.domain.userDomain.UserRedis;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class UserRedisConfiguration {
+
+  @Bean
+  ReactiveRedisOperations<String, UserRedis> redisOperations(ReactiveRedisConnectionFactory factory) {
+    Jackson2JsonRedisSerializer<UserRedis> serializer = new Jackson2JsonRedisSerializer<>(UserRedis.class);
+    RedisSerializationContext.RedisSerializationContextBuilder<String, UserRedis> builder = RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
+    RedisSerializationContext<String, UserRedis> context = builder.value(serializer).build();
+    return new ReactiveRedisTemplate<>(factory, context);
+  }
+}
