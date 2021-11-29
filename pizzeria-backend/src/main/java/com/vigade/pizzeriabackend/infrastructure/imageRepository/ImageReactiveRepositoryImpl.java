2972594f119468bfc7f4713e2fb2d831273461f7
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 03:09:47 +0100

    Create image redis get implementation in imagerepositoryimpl

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/imageRepository/ImageReactiveRepositoryImpl.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/imageRepository/ImageReactiveRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.vigade.pizzeriabackend.infrastructure.imageRepository;
 
 import java.time.Duration;
+import java.util.UUID;
 import com.vigade.pizzeriabackend.domain.imageDomain.Image;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
@@ -19,5 +20,17 @@ public class ImageReactiveRepositoryImpl implements ImageReactiveRepository {
     public Mono<Image> add(Image image) {
         return redisOperations.opsForValue().set(image.getId().toString(), image.getData(), Duration.ofDays(1))
                 .map(img -> image);
+    }
+
+    public Mono<Image> getImageRedis(UUID id){
+        return redisOperations.opsForValue()
+                              .get(id.toString())
+                              .flatMap(imageBytes -> {
+                                    Image image = new Image();
+                                    image.setData(imageBytes);
+                                    image.setId(id);
+                                    return Mono.just(image);
+                                }).
+                                onErrorResume(err -> Mono.error(new Exception()));
     }
 }
