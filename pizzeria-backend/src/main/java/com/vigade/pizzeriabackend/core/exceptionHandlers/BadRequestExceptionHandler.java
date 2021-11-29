d232f272dedd50814c1145ab279c282bc649456c
José Pascual Manzano Pérez <josepascual.manzano@gmail.com>
2021-11-29 20:23:35 +0100

    Logger functionality implemented.

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/exceptionHandlers/BadRequestExceptionHandler.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/exceptionHandlers/BadRequestExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.vigade.pizzeriabackend.core.exceptionHandlers;
 
 import com.vigade.pizzeriabackend.core.exceptions.BadRequestException;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class BadRequestExceptionHandler {
-    
+
+    private final Logger logger;
+
+    @Autowired
+    public BadRequestExceptionHandler(final Logger logger) {
+        this.logger = logger;
+    }
+
     @ExceptionHandler(value = {BadRequestException.class})
     protected ResponseEntity<Object> handleException(BadRequestException exception) {
+        logger.warn(String.format("%s , StackTrace: %s", exception.getMessage(), exception.getStackTrace().toString()));
         return ResponseEntity.status(exception.getStatusCode()).body(exception.getExceptions());
     }
 }
