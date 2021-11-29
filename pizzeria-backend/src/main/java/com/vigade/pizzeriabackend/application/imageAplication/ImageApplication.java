9bdea4fb1e9b6f5084ca0ccb02f79b1e827ee2a8
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 03:10:08 +0100

    Create get function in image application interface

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplication.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplication.java
@@ -1,8 +1,11 @@
 package com.vigade.pizzeriabackend.application.imageAplication;
 
+import java.util.UUID;
+
 import reactor.core.publisher.Mono;
 
 public interface ImageApplication {
     public Mono<ImageDTO> add(ImageDTOInput dto);
+    public Mono<ImageDTO> getImageRedis(UUID id);
 
 }
