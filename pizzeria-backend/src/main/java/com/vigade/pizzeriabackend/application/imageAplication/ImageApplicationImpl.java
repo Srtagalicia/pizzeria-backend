2a9acaecf7d34b67e2096497c9952a4a1acdce98
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 05:35:38 +0100

    implement logger in images creation

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplicationImpl.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/application/imageAplication/ImageApplicationImpl.java
@@ -2,30 +2,36 @@ package com.vigade.pizzeriabackend.application.imageAplication;
 
 import java.util.UUID;
 
+import com.vigade.pizzeriabackend.core.baseClasses.ApplicationBase;
 import com.vigade.pizzeriabackend.domain.imageDomain.Image;
 import com.vigade.pizzeriabackend.infrastructure.imageRepository.ImageReactiveRepository;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
 
 @Service
-public class ImageApplicationImpl implements ImageApplication {
+public class ImageApplicationImpl extends ApplicationBase<Image, UUID> implements ImageApplication {
     private final ImageReactiveRepository imageRepository;
     private final ModelMapper modelMapper;
+    private final Logger logger;
 
     @Autowired
-    public ImageApplicationImpl(final ImageReactiveRepository imageRepository, final ModelMapper modelMapper) {
+    public ImageApplicationImpl(final ImageReactiveRepository imageRepository, final ModelMapper modelMapper, final Logger logger) {
+        super((id) -> imageRepository.getImageRedis(id));
         this.imageRepository = imageRepository;
         this.modelMapper = modelMapper;
+        this.logger = logger;
     }
-
+//TODO: te deja añadir imagenes sin data
     public Mono<ImageDTO> add(ImageDTOInput dto) {
         Image image = modelMapper.map(dto, Image.class);
         image.setId(UUID.randomUUID());
         image.setThisNew(true);
+        this.logger.info(serializeObject(image, "Image Added"));
         return this.imageRepository.add(image)
                 .flatMap(monoImage -> Mono.just(this.modelMapper.map(monoImage, ImageDTO.class)));
     }
