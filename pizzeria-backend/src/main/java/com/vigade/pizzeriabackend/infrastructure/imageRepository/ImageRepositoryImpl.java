f7e7e8cac8c16807140c18d73cf6c04c4b9cd638
Fran Perea <franciscopereatora@gmail.com>
2021-11-29 13:35:44 +0100

    Refactor ImageRepositoryImpl

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/imageRepository/ImageRepositoryImpl.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/infrastructure/imageRepository/ImageRepositoryImpl.java
@@ -3,25 +3,29 @@ package com.vigade.pizzeriabackend.infrastructure.imageRepository;
 import java.time.Duration;
 import java.util.UUID;
 import com.vigade.pizzeriabackend.domain.imageDomain.Image;
+import com.vigade.pizzeriabackend.domain.imageDomain.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
-public class ImageReactiveRepositoryImpl implements ImageReactiveRepository {
+public class ImageRepositoryImpl implements ImageRepository {
+    
     private final ReactiveRedisOperations<String, byte[]> redisOperations;
 
     @Autowired
-    public ImageReactiveRepositoryImpl(final ReactiveRedisOperations<String, byte[]> redisOperations) {
+    public ImageRepositoryImpl(final ReactiveRedisOperations<String, byte[]> redisOperations) {
         this.redisOperations = redisOperations;
     }
 
+    @Override
     public Mono<Image> add(Image image) {
         return redisOperations.opsForValue().set(image.getId().toString(), image.getData(), Duration.ofDays(1))
                 .map(img -> image);
     }
 
+    @Override
     public Mono<Image> getImageRedis(UUID id){
         return redisOperations.opsForValue()
                               .get(id.toString())
