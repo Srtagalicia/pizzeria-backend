b43eadc63ce6a688ad45a3fe9044a1c9e4ebb8aa
frxnpt <62038055+frxnpt@users.noreply.github.com>
2021-11-29 05:34:50 +0100

    Implement logger in restresponse exception handler

--- pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/exceptionHandlers/RestResponseEntityExceptionHandler.java
+++ pizzeria-backend/src/main/java/com/vigade/pizzeriabackend/core/exceptionHandlers/RestResponseEntityExceptionHandler.java
@@ -2,22 +2,38 @@ package com.vigade.pizzeriabackend.core.exceptionHandlers;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler {
-    
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    protected ResponseEntity<Object> handleException(MethodArgumentNotValidException exception) {
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final Logger logger;
+
+    @Autowired
+    public RestResponseEntityExceptionHandler(final Logger logger) {
+        this.logger = logger;
+    }
+
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    protected ResponseEntity<Object> handleException(MethodArgumentNotValidException exception, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
         Map<String, String> errors = new HashMap<>();
-        for(FieldError error : exception.getFieldErrors()) {
+        for (FieldError error : exception.getFieldErrors()) {
             errors.put(error.getField(), error.getDefaultMessage());
         }
+        logger.warn(String.format("%s , StackTrace: %s", exception.getMessage(), exception.getStackTrace().toString()));
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
